b9a77d9d91598451999f33efd1ed116a3bc92d60
Giovanni Sacchitiello <sacchitiellogiovanni@gmail.com>
2022-03-23 22:06:12 -0300

    trabalhando com lists e Remove

--- Program.cs
+++ Program.cs
@@ -14,20 +14,47 @@
 // Seu programa deve preencher as informações(dados do usuário, carrinho e produtos de acordo com o input do usuário) - vide vídeo de exemplo
 using ProjetoHeranca.Animais;
 using ProjetoHeranca.LojaVirtual;
-namespace projetoheranca
+namespace ProjetoHeranca
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Animal[] animais = new Animal[3]; 
-            animais[0] = new Cachorro("Pitbull", 3, 15);
-            animais[1] = new Hamster("RacaDoHamster", 2, 2);
-            animais[2] = new Gato("Siames", 2, 5);
-            Pedido pedido = new Pedido("Giovanni", "11991392711", animais);
-            Console.WriteLine(pedido.CalcularValor());
+            var numeros = new List<int>();
+            var numerosMaisDez = new List<int>();
+            var strings = new List<string>();
+            for(var contador = 0; contador < 10; contador++){
+                numeros.Add(contador+2);
+                numerosMaisDez.Add(contador+12);
+                strings.Add(contador.ToString());
+            }
+
+            //Adicionar outra lista
+            numeros.AddRange(numerosMaisDez);
+            //Remover Item
+            var tentativaUm = numeros.Remove(5); //Remove o valor independente da posição e devolve se removeu
+            numeros.RemoveAt(3); //Remove no indice escolhido
+            numeros.RemoveAll(numeroARemover => numeroARemover < 10 || numeroARemover > 14); //Remove todos os itens que atendam o predicado
+            numeros.RemoveRange(0, 5); //Remove itens em sequencia, parametros a primeira posição e a quantidade
+            //Buscar Index
+            
+            //Buscar ultimo
+
+            //Buscar na lista
+
+            //Ordenação
+
+            
+            Console.WriteLine("Finalizou");
         }
 
+
+            //Animal[] animais = new Animal[3]; 
+            // animais[0] = new Cachorro("Pitbull", 3, 15);
+            // animais[1] = new Hamster("RacaDoHamster", 2, 2);
+            // animais[2] = new Gato("Siames", 2, 5);
+            // Pedido pedido = new Pedido("Giovanni", "11991392711", animais);
+            // Console.WriteLine(pedido.CalcularValor());
 
         //Prato[] pratos = new Prato[4];
         // pratos[0] = new Pizza("Portuguesa", true); // 40
