516e532bde919b1f54358e9852e14daaffd9cf73
Giovanni Sacchitiello <sacchitiellogiovanni@gmail.com>
2022-03-24 19:28:05 -0300

    avancando nas lists c#

--- Program.cs
+++ Program.cs
@@ -20,6 +20,9 @@ namespace ProjetoHeranca
     {
         public static void Main(string[] args)
         {
+            var nomes = new List<string>();
+            var idades = new List<int>?();
+
             var numeros = new List<int>();
             var numerosMaisDez = new List<int>();
             var strings = new List<string>();
@@ -32,18 +35,26 @@ namespace ProjetoHeranca
             //Adicionar outra lista
             numeros.AddRange(numerosMaisDez);
             //Remover Item
-            var tentativaUm = numeros.Remove(5); //Remove o valor independente da posição e devolve se removeu
-            numeros.RemoveAt(3); //Remove no indice escolhido
-            numeros.RemoveAll(numeroARemover => numeroARemover < 10 || numeroARemover > 14); //Remove todos os itens que atendam o predicado
-            numeros.RemoveRange(0, 5); //Remove itens em sequencia, parametros a primeira posição e a quantidade
+            // var tentativaUm = numeros.Remove(5); //Remove o valor independente da posição e devolve se removeu
+            // numeros.RemoveAt(3); //Remove no indice escolhido
+            // numeros.RemoveAll(numeroARemover => numeroARemover < 10 || numeroARemover > 14); //Remove todos os itens que atendam o predicado
+            // numeros.RemoveRange(0, 5); //Remove itens em sequencia, parametros a primeira posição e a quantidade
             //Buscar Index
-            
-            //Buscar ultimo
-
+            var indexString = strings.IndexOf("Giovanni");
+            var indexCinco = numeros.IndexOf(5); //Busca a posição na lista de um determinado valor
+            //Desafio: Fazer o IndexOf na mão utilizando vetores(arrays)
             //Buscar na lista
-
+            var valorNaPosicaoTres = numeros.ElementAt(3); //Buscar o item na posição informada
+            var valorPar = numeros.Find(numero => numero % 2 == 0); //Busca o primeiro item que atenda uma posição
+            var valoresParesDois = numeros.FindAll(numero => numero % 2 == 0); //Busca todos os itens que atendam uma condição 
+            //Verificação
+            var temQuinze = numeros.Any(numero => numero == 15);//Me confirma se existe qualquer item que atenda minha condição
+            var temQuartorze = numeros.Exists(x => x == 14); //Me confirma se existe qualquer item que atenda minha condição
+            var todosSaoPares = numeros.All(numero => numero % 2 == 0); //Verifica se TODOS o itens da lista atendem minha condição
+            var textoComMaisDeCincoLetras = strings.All(numero => strings.Count > 5);//Verifica se TODOS o itens da lista atendem minha condição
             //Ordenação
-
+            var ordenadoDescendente = numeros.OrderByDescending(x => x); // Retorna uma nova lista de forma descrescente
+            var ordenadoCrescente = numeros.OrderBy(x => x); // Retorna uma nova lista de forma crescente
             
             Console.WriteLine("Finalizou");
         }
