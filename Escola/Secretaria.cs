267e85ef52c1de3944352aa09cd88e516d5ba14c
Giovanni Sacchitiello <sacchitiellogiovanni@gmail.com>
2022-03-24 23:02:00 -0300

    criando secretaria e cadastrando salas

---
+++ Escola/Secretaria.cs
@@ -0,0 +1,51 @@
+namespace ProjetoHeranca.Escola
+{
+    public class Secretaria
+    {
+        public void CadastrarNovasTurmas()
+        {
+            var salasDeAula = new List<SalaDeAula>();
+            var opcaoContinuar = 0;
+            do
+            {
+                Console.WriteLine("Deseja adicionar uma nova sala de aula? 1 = S | 2 = N");
+                opcaoContinuar = int.Parse(Console.ReadLine());
+                if (opcaoContinuar == 1)
+                {
+                    Console.WriteLine("Qual o nome do professor?");
+                    var nomeProfessor = Console.ReadLine();
+                    Console.WriteLine("Qual a serie?");
+                    var serie = Console.ReadLine();
+                    var alunos = CadastrarPrimeirosAlunosDaTurma();
+                    salasDeAula.Add(new SalaDeAula(serie, alunos, nomeProfessor));
+                }
+            } while (opcaoContinuar != 2);
+            Console.WriteLine($"Voce criou {salasDeAula.Count}");
+        }
+        private List<Aluno> CadastrarPrimeirosAlunosDaTurma()
+        {
+            var continuarAdicionando = 0;
+            Console.WriteLine("Deseja adicionar novos alunos? 1= S | 2 = N");
+            continuarAdicionando = int.Parse(Console.ReadLine());
+            var primeirosAlunos = new List<Aluno>();
+            while (continuarAdicionando == 1)
+            {
+                primeirosAlunos.Add(CadastrarNovoAluno());
+                Console.WriteLine("Deseja adicionar novos alunos? 1= S | 2 = N");
+                continuarAdicionando = int.Parse(Console.ReadLine());
+            }
+            return primeirosAlunos;
+        }
+
+        private Aluno CadastrarNovoAluno()
+        {
+            Console.WriteLine("Qual o nome do aluno? ");
+            var nomeAluno = Console.ReadLine();
+            Console.WriteLine("Qual o CPF do aluno? ");
+            var cpfAluno = Console.ReadLine();
+            Console.WriteLine("Qual a data de nascimento? ");
+            var dataNascimentoAluno = DateTime.Parse(Console.ReadLine());
+            return new Aluno(nomeAluno, dataNascimentoAluno, cpfAluno);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@
 //Para a criação do metódo AdicionarItem voce precisará verificar maior posição sem item do array(para nao adicionar numa posição que já tem item)
 // Seu programa deve preencher as informações(dados do usuário, carrinho e produtos de acordo com o input do usuário) - vide vídeo de exemplo
 using ProjetoHeranca.Animais;
+using ProjetoHeranca.Escola;
 using ProjetoHeranca.LojaVirtual;
 namespace ProjetoHeranca
 {
@@ -20,45 +21,50 @@ namespace ProjetoHeranca
     {
         public static void Main(string[] args)
         {
-           
+            var secretaria = new Secretaria();
+            secretaria.CadastrarNovasTurmas();
         }
 
 
+        public static void ExerciciosLista()
+        {
+            var nomes = new List<string>();
+            var idades = new List<int>();
+            var numeros = new List<int>();
+            var numerosMaisDez = new List<int>();
+            var strings = new List<string>();
+            for (var contador = 0; contador < 10; contador++)
+            {
+                numeros.Add(contador + 2);
+                numerosMaisDez.Add(contador + 12);
+                strings.Add(contador.ToString());
+            }
 
-        // var nomes = new List<string>();
-        //         var idades = new List<int>();
-        //         var numeros = new List<int>();
-        //         var numerosMaisDez = new List<int>();
-        //         var strings = new List<string>();
-        //         for(var contador = 0; contador < 10; contador++){
-        //             numeros.Add(contador+2);
-        //             numerosMaisDez.Add(contador+12);
-        //             strings.Add(contador.ToString());
-        //         }
+            //Adicionar outra lista
+            numeros.AddRange(numerosMaisDez);
+            //Remover Item
+            // var tentativaUm = numeros.Remove(5); //Remove o valor independente da posição e devolve se removeu
+            // numeros.RemoveAt(3); //Remove no indice escolhido
+            // numeros.RemoveAll(numeroARemover => numeroARemover < 10 || numeroARemover > 14); //Remove todos os itens que atendam o predicado
+            // numeros.RemoveRange(0, 5); //Remove itens em sequencia, parametros a primeira posição e a quantidade
+            //Buscar Index
+            var indexString = strings.IndexOf("Giovanni");
+            var indexCinco = numeros.IndexOf(5); //Busca a posição na lista de um determinado valor
+                                                 //Desafio: Fazer o IndexOf na mão utilizando vetores(arrays)
+                                                 //Buscar na lista
+            var valorNaPosicaoTres = numeros.ElementAt(3); //Buscar o item na posição informada
+            var valorPar = numeros.Find(numero => numero % 2 == 0); //Busca o primeiro item que atenda uma posição
+            var valoresParesDois = numeros.FindAll(numero => numero % 2 == 0); //Busca todos os itens que atendam uma condição 
+                                                                               //Verificação
+            var temQuinze = numeros.Any(numero => numero == 15);//Me confirma se existe qualquer item que atenda minha condição
+            var temQuartorze = numeros.Exists(x => x == 14); //Me confirma se existe qualquer item que atenda minha condição
+            var todosSaoPares = numeros.All(numero => numero % 2 == 0); //Verifica se TODOS o itens da lista atendem minha condição
+            var textoComMaisDeCincoLetras = strings.All(numero => strings.Count > 5);//Verifica se TODOS o itens da lista atendem minha condição
+                                                                                     //Ordenação
+            var ordenadoDescendente = numeros.OrderByDescending(x => x); // Retorna uma nova lista de forma descrescente
+            var ordenadoCrescente = numeros.OrderBy(x => x); // Retorna uma nova lista de forma crescente
+        }
 
-        //         //Adicionar outra lista
-        //         numeros.AddRange(numerosMaisDez);
-        //         //Remover Item
-        //         // var tentativaUm = numeros.Remove(5); //Remove o valor independente da posição e devolve se removeu
-        //         // numeros.RemoveAt(3); //Remove no indice escolhido
-        //         // numeros.RemoveAll(numeroARemover => numeroARemover < 10 || numeroARemover > 14); //Remove todos os itens que atendam o predicado
-        //         // numeros.RemoveRange(0, 5); //Remove itens em sequencia, parametros a primeira posição e a quantidade
-        //         //Buscar Index
-        //         var indexString = strings.IndexOf("Giovanni");
-        //         var indexCinco = numeros.IndexOf(5); //Busca a posição na lista de um determinado valor
-        //         //Desafio: Fazer o IndexOf na mão utilizando vetores(arrays)
-        //         //Buscar na lista
-        //         var valorNaPosicaoTres = numeros.ElementAt(3); //Buscar o item na posição informada
-        //         var valorPar = numeros.Find(numero => numero % 2 == 0); //Busca o primeiro item que atenda uma posição
-        //         var valoresParesDois = numeros.FindAll(numero => numero % 2 == 0); //Busca todos os itens que atendam uma condição 
-        //         //Verificação
-        //         var temQuinze = numeros.Any(numero => numero == 15);//Me confirma se existe qualquer item que atenda minha condição
-        //         var temQuartorze = numeros.Exists(x => x == 14); //Me confirma se existe qualquer item que atenda minha condição
-        //         var todosSaoPares = numeros.All(numero => numero % 2 == 0); //Verifica se TODOS o itens da lista atendem minha condição
-        //         var textoComMaisDeCincoLetras = strings.All(numero => strings.Count > 5);//Verifica se TODOS o itens da lista atendem minha condição
-        //         //Ordenação
-        //         var ordenadoDescendente = numeros.OrderByDescending(x => x); // Retorna uma nova lista de forma descrescente
-        //         var ordenadoCrescente = numeros.OrderBy(x => x); // Retorna uma nova lista de forma crescente
 
         //Animal[] animais = new Animal[3]; 
         // animais[0] = new Cachorro("Pitbull", 3, 15);
